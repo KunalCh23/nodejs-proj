9572d61644073e23ed00cc9fd7e306beccc3ff05
Kunal Chaudhary <98645631+KunalCh23@users.noreply.github.com>
2023-09-21 15:51:26 +0530

    Update before_install.sh

--- scripts/before_install.sh
+++ scripts/before_install.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 #download node and npm
-curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.3/install.sh | bash
-source ~/.bashrc
-nvm install v16.0.0
+# curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.3/install.sh | bash
+# source ~/.bashrc
+# nvm install v16.0.0
 
 #create our working directory if it doesnt exist
 DIR="/home/ubuntu/myapp"
